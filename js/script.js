80216f7f72388dcb76418535a415bd4b295b04da
Itamar <ivoitamar@gmail.com>
2021-11-08 10:50:11 -0300

    Criação do movimento de descida.

--- js/script.js
+++ js/script.js
@@ -10,11 +10,27 @@ function jump() {
     let position = 0;
 
     let upInterval = setInterval(() => {
-        position += 20;
-        
-        dino.style.bottom = position + 'px';
+        if (position >= 150) {
+            clearInterval(upInterval);
 
+            let downInterval = setInterval(() => {
+                if (position <= 0) {
+                    clearInterval(downInterval);
+                } else {
+
+                    position -= 20;
+
+                    dino.style.bottom = position + 'px';
+                }
+            }, 20);
+        } else {
+
+            position += 20;
+
+            dino.style.bottom = position + 'px';
+        }
     }, 20);
 }
+
 
 document.addEventListener('keyup', handleKeyUp);
