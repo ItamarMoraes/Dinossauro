404a5577d8de3f24f58c655f4844cc5fbb195501
Itamar <ivoitamar@gmail.com>
2021-11-08 10:07:21 -0300

    Criação da função de detecção da tecla de espaço

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
 ﻿const dino = document.querySelector('.dino');
+
+function handleKeyUp(event) {
+    if (event.keyCode === 32) {
+        console.log("Você precionou espaço");
+    }
+}
+
+document.addEventListener('keyup', handleKeyUp);
